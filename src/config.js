d5136a8ad4dd6c7f3c3ffd481094a685de860f0f
Andrew Domanski <andqok@gmail.com>
2024-03-18 15:35:09 +0200

    update password min length

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const apiEndpoint = 'https://auth-qa.qencode.com';
 
 // export const apiEndpoint = 'http://localhost:3001'
 export const brandName = 'Qencode';
-export const passwordMinLength = 5;
+export const passwordMinLength = 8;
 export const passwordMaxLength = 512;
